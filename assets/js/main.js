e7f6572308f5b0c913e0e7b604bfda73912ce8db
tuananh141101 <83608711+tuananh141101@users.noreply.github.com>
2023-05-05 11:55:48 +0700

    update menu-mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,19 @@ const headerScroll = document.querySelector('.header');
 window.addEventListener('scroll', () => {
     headerScroll.classList.add("active-header-scroll");
 });
+
+// Overlay-menu
+const overlayMenu = document.querySelector('.overlay-menu');
+const barIcon = document.querySelector('.bars-icon');
+const closeOverlay = document.querySelector('.overlay-menu p');
+
+barIcon.addEventListener('click', () => {
+    overlayMenu.classList.add("active-overlay");
+    barIcon.style.display = "none";
+});
+
+closeOverlay.addEventListener('click', ()=> {
+    overlayMenu.classList.remove('active-overlay');
+    barIcon.style.display = "block";
+})
+
