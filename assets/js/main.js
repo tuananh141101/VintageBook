1a354a7387d3e7aa2461875481950c5a305010a5
tuananh141101 <83608711+tuananh141101@users.noreply.github.com>
2023-05-10 15:48:43 +0700

    update products-bestselling/sale

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,11 +45,15 @@ closeOverlay.addEventListener('click', ()=> {
 // product-slider
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    margin:20,
     nav:true,
+    dots:false,
     responsive:{
         0:{
             items:1
+        },
+        500:{
+            items:2
         },
         600:{
             items:3
