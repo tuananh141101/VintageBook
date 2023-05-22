33625695bf31945c9a549831b7d8a5b2e94d3b27
tuananh141101 <83608711+tuananh141101@users.noreply.github.com>
2023-05-22 08:48:20 +0700

    update event/detailevent js

---
+++ assets/js/detailEvent.js
@@ -0,0 +1,99 @@
+let listEvent = [
+    {
+        id: 1,
+        name: 'The autobiography "Sinh nam 1972" by Alpha Books Chairman Nguyen Canh Binh',
+        date:'7 May 2022',
+        title: 'Cuốn tự truyện kể về toàn bộ quá trình học tập, trưởng thành và phát triển của bản thân ông Nguyễn Cảnh Bình kể từ những ngày thơ bé cho đến nay.Chia sẻ về cuốn tự truyện ông Bình cho biết, ban đầu, ông chỉ dự định kể lại những chuyện xảy ra trong cuộc đời, để chiêm nghiệm lại những gì ông đã trải nghiệm với Alpha Books, với việc đọc sách, việc kinh doanh, xã hội, con người, bạn bè... Nhưng rồi cuốn sách đã được mở rộng ra. Và thấp thoáng trong cuốn sách còn có cả câu chuyện của những người bạn đồng trang lứa, những mong ước, dự định ông muốn thực hiện trong những năm tháng tiếp theo của cuộc đời.2000 bản in trong lần phát hành đầu tiên đã đến tay độc giả và nhận được nhiều đánh giá tích cực. Cuốn tự truyện được đánh giá là "Bức chân dung đầy đủ, mạnh mẽ, đầy lôi cuốn, truyền cảm hứng sống và cảm hứng viết cho tất thảy những ai muốn sống một cuộc đời không uổng phí" và là "cuốn tự truyện sinh động và gần gũi về một cuộc đời đầy màu sắc được kể bằng tất cả trái tim, với rất nhiều niềm vui, nỗi buồn, đắng cay, hạnh phúc. Trên tất cả là ý chí sống mãnh liệt của một con người không bao giờ cho phép mình dừng lại',
+        img: 'http://127.0.0.1:5500/assets/img/events-1.jpg'
+    },
+
+    {
+        id: 2,
+        name: 'The writer Nguyễn Phan Quế Mai has recently released a new English novel called "Dust Child".',
+        date: 'March 14 2023',
+        title: 'Tiểu thuyết tiếng Anh mới của nhà văn đa tài.Trên tài khoản mạng xã hội của cá nhân, nhà văn - nhà thơ- dịch giả Nguyễn Phan Quế Mai thông báo: cuốn tiểu thuyết tiếng Anh thứ hai của cô Dust Child (Bụi đời) vừa chính thức được phát hành tạo Mỹ vào ngày 14/3/2023 vừa qua.Nhà văn Nguyễn Phan Quế Mai cũng tham gia 23 sự kiện tổ chức tại 19 thành phố trên nước Mỹ trong thời gian từ 14/3-8/4, để quảng bá cuốn tiểu thuyết.Ngay trước khi được chính thức phát hành, cuốn tiểu thuyết Dust Child của nữ nhà văn Việt kiều đã được điểm tên trong Top sách hay nhất tháng 3, Top sách hay nhất Xuân 2023 của các tờ báo, trang thông tin Los Angeles Times, Apple Books, Chicago Review of Books, Ms. Magazine, BookPage BookBub và Amazon. Các nhà bán sách độc lập trên khắp Hoa Kỳ cũng đã đưa cuốn sách vào trong danh sách Indie Next Great Read.Một lần nữa, độc giả yêu thích nhà văn Nguyễn Phan Quế Mai lại có thêm cơ hội tự hào và hồi hộp dõi theo hành trình chinh phục trái tim độc giả thế giới của nữ nhà văn cũng như tác phẩm mới Dust Child của cô.',
+        img: 'https://static.tramdoc.vn/image/img.news/0/0/0/11976.jpg?v=1&w=628&h=365&nocache=1'
+    },
+
+    {
+        id: 3,
+        name: 'Arrange an author talk for the book "Áo tưởng của sự bất khả chiến bại',
+        date: '7 may 2022',
+        title: 'Andreas Krebs và Paul Williams là hai tác giả nổi tiếng người Đức và là nhà lãnh đạo doanh nghiệp có nhiều kinh nghiệm trong lĩnh vực quản trị. Hai ông đã cùng nhau viết nên cuốn sách về quản trị doanh nghiệp The Illusion Of Invincibility, được Tân Việt Books mua bản quyền và xuất bản tiếng Việt với tựa đề Ảo tưởng của sự bất khả chiến bại.Trong đó, Andreas Krebs là một doanh nhân, một nhà quản lý có kinh nghiệm quốc tế và một chuyên gia về lãnh đạo toàn cầu hóa. Ông là một trong số ít người Đức lọt vào ban điều hành của tập đoàn dược phẩm Big Pharma trụ sở tại Mỹ. Andreas hiện đang điều hành doanh nghiệp đầu tư của riêng mình Longfield Invest, chuyên đầu tư vào các doanh nghiệp mới thành lập và doanh nghiệp tăng trưởng bao trùm nhiều lĩnh vực hoạt động trong nền kinh tế mới.Andreas từng quản lý hơn 8.000 nhân viên ở 96 quốc gia. Ông còn là CEO tại công ty dược phẩm Merz KgaA (Frankfurt) và giữ các vị trí khác trong hội đồng quản trị trong nhiều lĩnh vực khác nhau...',
+        img: 'http://static.tramdoc.vn/image/img.news/0/0/0/11770.jpg?v=1&w=628&h=365&nocache=1'
+    },
+
+    {
+        id:4,
+        name: 'The new book "Những người hàng xóm" by writer Nguyen Nhat Anh has been released.',
+        date: '14 December 2022',
+        title: 'heo như tiết lộ của nhà văn Nguyễn Nhật Ánh, cơ duyên để ông viết nên tác phẩm Những người hàng xóm là do được “rong chơi ở nước ngoài quá xá lâu", tận hơn hai tháng. Trong thời gian ấy, ông đã đi thăm các danh lam thắng cảnh nổi tiếng, khám phá nền văn hóa độc đáo nơi xứ người. Và chính rảnh rỗi sinh... nông nổi, nên ông bắt tay vào viết một cuốn truyện dài có bối cảnh và những nhân vật hoàn toàn mới.Đây cũng là một sự thay đổi đề tài của nhà văn khi độc giả đã quen với những tác phẩm nói về tuổi thơ và dòng chảy cảm xúc của tuổi mới lớn trong suốt 40 năm viết sách của ông. Ông cũng cho biết, việc viết truyện theo một đề tài khác chính là thách thức và cơ hội để khám phá những điều mới mẻ.Nhà văn không viết về chuyện tình của một người mà viết về rất nhiều câu chuyện thấm đẫm tình người: Đó là một anh chàng mới lấy vợ, rất yêu vợ, đang chờ đi làm và thích viết lách loăng quăng. Đó là câu chuyện tình của anh cảnh sát với cô bạn gái ngành y; là mối tình nở muộn của cô gái dẫn chương trình truyền hình bằng ngôn ngữ ký hiệu; là tình cảm xúc-động-mà-ngô-nghê của anh chàng chủ hãng dao cạo dành cho mẹ mình; và đặc biệt là mối tình khôn nguôi của ông họa sĩ già thương nhớ người vợ quá cố - người phụ nữ xinh đẹp, người mẫu, nàng thơ của ông.Trong tác phẩm của mình, Nguyễn Nhật Ánh không hướng trọng tâm vào câu chuyện của một người mà đan cài nhiều câu chuyện để đưa tới một thông điệp xuyên suốt: Cuộc sống, với dòng chảy vui buồn tưởng như bất tận, vẫn thật đẹp khi ta để tâm ngắm nhìn. Khép lại cuốn sách, có lẽ bạn đọc sẽ thấy trong mình một niềm vui bình yên.',
+        img: 'http://static.tramdoc.vn/image/img.news/0/0/0/11770.jpg?v=1&w=628&h=365&nocache=1'
+    },
+
+    {
+        id:5,
+        name: 'The Ministry of Culture, Sports, and Tourism has issued a plan to organize the Second Vietnam Book and Reading Culture Day.',
+        date: 'Mid-March to the end of April 2023',
+        title: 'Theo đó, việc tổ chức Ngày Sách và Văn hóa đọc Việt Nam nhằm góp phần thực hiện chủ trương, chính sách của Đảng và Nhà nước về phát triển văn hóa đọc, triển khai Nghị quyết Đại hội XIII của Đảng về xây dựng phát huy giá trị văn hóa, sức mạnh con người Việt Nam; khẳng định vị trí, vai trò, tầm quan trọng của sách đối với việc nâng cao kiến thức, kỹ năng, rèn luyện nhân cách con người; khuyến khích phong trào đọc sách; tôn vinh các tổ chức, cá nhân có đóng góp cho phát triển sách và văn hóa đọc.Đây cũng là dịp để lan tỏa văn hóa đọc trong đội ngũ công chức, viên chức và người lao động của Bộ Văn hóa, Thể thao và Du lịch nói riêng và trong toàn ngành nói chung, góp phần xây dựng hệ giá trị văn hóa và chuẩn mực con người Việt Nam.Theo kế hoạch, Ngày Sách và Văn hóa đọc Việt Nam lần thứ hai sẽ được triển khai với các nội dung: Tuyên truyền về giá trị, ý nghĩa của Ngày Sách và Văn hóa đọc Việt Nam gắn với việc phát triển văn hóa đọc trong cộng đồng, gắn với thông điệp: “Sách: Nhận thức - Đổi mới - Sáng tạo”, “Sách cho tôi, cho bạn”; tổ chức sự kiện Ngày hội sách và văn hóa đọc với các hoạt động tuyên truyền giới thiệu sách, kể chuyện theo sách, vẽ tranh theo sách, xếp sách nghệ thuật; giao lưu, tọa đàm về sách và văn hóa đọc; tôn vinh người làm thư viện, lưu giữ, sưu tầm, quảng bá sách và góp phần phát triển văn hóa đọc; quyên góp, ủng hộ và trao tặng sách; hướng dẫn kỹ năng đọc sách...Các hoạt động được tổ chức từ giữa tháng 3 đến hết tháng 4, trọng điểm từ ngày 15 đến 21/4/ 2023.',
+        img: 'http://static.tramdoc.vn/image/img.news/0/0/0/11942.jpg?v=1&w=628&h=365&nocache=1'
+    },
+
+    {
+        id:6,
+        name: 'On December 19th, the Nguyen Van Binh Book Street won the highest prize in the IFLA AAPME Awards 2022, in the Economic Potential category.',
+        date: 'December 19 2023',
+        title: 'Liên đoàn Kiến trúc sư Cảnh quan Quốc tế (IFLA) nhận xét về công trình Đường sách Nguyễn Văn Bình, quận 1, TP.HCM như sau: "Đây là một thiết kế sáng tạo hiếm có, nhỏ và đẹp, có vai trò kiến tạo kinh tế không nhỏ cho một TP.HCM đang chuyển mình.Sau gần 7 năm đường sách Nguyễn Văn Bình đi vào hoạt động, đây là lần đầu tiên TS Vũ Việt Anh - kiến trúc sư đã đồng hành cùng Đường sách Nguyễn Văn Bình từ những ngày đầu tiên - gửi hồ sơ của đường sách tham dự một giải thưởng quốc tế và nhận được giải thưởng cao.Anh chia sẻ: “Nhìn chung Đường sách Nguyễn Văn Bình có quy mô khá nhỏ so với các dự án khác. Tuy nhiên nó vẫn được đánh giá cao bởi quan điểm tiếp cận và ý nghĩa văn hóa mà nó mang lại.Ông Lê Hoàng - Phó chủ tịch Hội Xuất bản Việt Nam, Giám đốc Công ty Đường sách TP.HCM - đánh giá cao không gian xanh tại đây với hàng cây cổ thụ dọc hai bên đường sách như một món quà của tự nhiên. “Những người đến đường sách không chỉ để mua sách, mà còn có thể ngồi đọc, tham gia các hoạt động liên quan đến sách hoặc đến ngồi quán cafe nhưng với một không khí văn hóa rất khác”, ông chia sẻ.Dưới bóng của hàng cây cổ thụ, Đường sách Nguyễn Văn Bình là nơi độc giả có thể dừng lại đọc sách bất kỳ lúc nào trong ngày. Các quán cafe tại đây cũng duy trì không gian mở, dù không gắn điều hòa vẫn có nhiều người đến để đọc sách và làm việc.',
+        img: 'http://static.tramdoc.vn/image/img.news/0/0/0/11763.jpg?v=1&w=628&h=365&nocache=1'
+    }
+];
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+console.log(id);
+
+function nameDetail(listEvent) {
+    let html = ``;
+    listEvent.forEach((item) => {
+        if (item.id == id) {
+            html +=
+            `
+            <a href="" class="name-book" id="event_Name">${item.name}</a>
+            `
+        }
+    });
+    const nameEvent = document.getElementById("nameEvent");
+    nameEvent.innerHTML = html;
+};
+nameDetail(listEvent)
+
+function detailEvent(listEvent) {
+    let html = ``;
+
+    listEvent.forEach((item) => {
+        if (item.id == id) {
+            html +=
+        `
+        <div class="img">
+                <img src="${item.img}"image not found">
+            </div>
+
+            <div class="content">
+
+                <div class="name">
+                    <span>${item.name}</span>
+                </div>
+
+                <div class="date"><span>${item.date}</span></div>
+
+                <div class="title">
+                    <span>${item.title}</span>
+                </div>
+            </div>
+        `;
+        };
+    });
+    const detaileventContainer = document.querySelector('.detailevent__container');
+    detaileventContainer.innerHTML = html;
+};
+detailEvent(listEvent);
